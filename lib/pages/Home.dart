2f4a3ee02dfa4faabba49fe088fd9336806a0d22
Hope Dexter Madziakapita <48337899+hopemadziakapita@users.noreply.github.com>
2024-01-25 22:49:53 +0200

    Update Home.dart
    pageview

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -7,89 +7,105 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int index = 0;
     return Scaffold(
       backgroundColor: AppColors().bgColor,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 20.0),
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          border: Border.fromBorderSide(
-                            BorderSide(
-                                color: AppColors().primaryColor, width: 2),
-                          ),
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(100),
-                          ),
-                        ),
-                        child: const Icon(Icons.person),
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      const Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Hope",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 18,
-                              ),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            border: Border.fromBorderSide(
+                              BorderSide(
+                                  color: AppColors().primaryColor, width: 2),
                             ),
-                            Text(
-                              "welcome back",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black26),
-                            )
-                          ],
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(100),
+                            ),
+                          ),
+                          child: const Icon(Icons.person),
                         ),
-                      )
-                    ],
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        "My tasks",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 20,
+                        const SizedBox(
+                          width: 20,
                         ),
-                      )
-                    ],
+                        const Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Hope",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              Text(
+                                "welcome back",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black26),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 10),
-                  child: Row(
-                    children: [
-                      FilterTab(
-                          title: "Recently", callback: () {}, isActive: true),
-                      FilterTab(
-                          title: "Today", callback: () {}, isActive: false),
-                      FilterTab(
-                          title: "Upcoming", callback: () {}, isActive: false),
-                      FilterTab(
-                          title: "Later", callback: () {}, isActive: false),
-                    ],
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "My tasks",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 20,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0, vertical: 10),
+                    child: Row(
+                      children: [
+                        FilterTab(
+                            title: "Recently", callback: () {}, isActive: true),
+                        FilterTab(
+                            title: "Today", callback: () {}, isActive: false),
+                        FilterTab(
+                            title: "Upcoming",
+                            callback: () {},
+                            isActive: false),
+                        FilterTab(
+                            title: "Later", callback: () {}, isActive: false),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: PageView(
+                      scrollDirection: Axis.horizontal,
+                      controller: PageController(initialPage: index),
+                      onPageChanged: (value) {
+                        index = value;
+                      },
+                      children: [Text("Recent"), Text("data")],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
